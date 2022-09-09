f3a3d11b265a6ac9157b9e218ad06ead15cf710c
Ohad Cohen <ohad909881122@gmail.com>
2022-09-09 14:25:08 +0300

    U

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,16 +9,16 @@ import { BsLinkedin } from "react-icons/bs";
 
 const Footer = () => {
 
-  const countEl = document.getElementById('count');
-  countVisits();
-  function countVisits() {
-    fetch('https://api.countapi.xyz/update/laptop/mouse/?amount=1')
-    .then((res) => res.json())
-    .then((res) => {
-      countEl.innerHTML = res.value;
-    });
-  }
-  
+  // const countEl = document.getElementById('count');
+  // countVisits();
+  // function countVisits() {
+  //   fetch('https://api.countapi.xyz/update/laptop/mouse/?amount=1')
+  //   .then((res) => res.json())
+  //   .then((res) => {
+  //     countEl.innerHTML = res.value;
+  //   });
+  // }
+
   return (
     <footer>
       <a href="#" className="footer__logo">
