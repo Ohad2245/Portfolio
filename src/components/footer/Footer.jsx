ea18a31619caf56fbf7319acc58107c07cb1b9c0
Ohad Cohen <ohad909881122@gmail.com>
2022-09-09 14:28:21 +0300

    U

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,15 +9,15 @@ import { BsLinkedin } from "react-icons/bs";
 
 const Footer = () => {
 
-  // const countEl = document.getElementById('count');
-  // countVisits();
-  // function countVisits() {
-  //   fetch('https://api.countapi.xyz/update/laptop/mouse/?amount=1')
-  //   .then((res) => res.json())
-  //   .then((res) => {
-  //     countEl.innerHTML = res.value;
-  //   });
-  // }
+  const countEl = document.getElementById('count');
+  countVisits();
+  function countVisits() {
+    fetch('https://api.countapi.xyz/update/laptop/mouse/?amount=1')
+    .then((res) => res.json())
+    .then((res) => {
+      countEl.innerHTML = res.value;
+    });
+  }
 
   return (
     <footer>
